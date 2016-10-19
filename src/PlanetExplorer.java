fd28438e2094cd1a71ca098191e7921840de7f09
somename <someemail>
2016-10-19 16:02:26 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -29,6 +29,8 @@ public class PlanetExplorer {
 	
 	public String executeCommand(String command){
 
+			int currPos_X;
+			int currPos_Y;
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
 		 * Example: 
 		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
