cc776960c562a1a1510eaa67819981d231d16177
somename <someemail>
2016-10-19 14:34:22 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 public class TestPlanetExplorer {
 	
+	PlanetExplorer explorer = new PlanetExplorer(0,0,"");
 
 	@Test
 	public void test_initializeState() throws PlanetExplorerException {
 	assertNotNull(explorer);
+	}
+	
+	@Test
+	public void test_explorerFacingNorth() throws PlanetExplorerException {
+	assertEquals("North", explorer.getFacing());
 	}
 }
